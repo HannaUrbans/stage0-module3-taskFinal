693e351005d02b6beaf1b09a288a539a2a5691b8
Hanna_Urban <huan92@yandex.ru>
2024-01-16 01:01:36 +0300

    done

--- src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumbersPrinter.java
@@ -1,7 +1,12 @@
 package lang.print.gaps.finalModuleTask;
 
 public class NumbersPrinter {
-    public void printIsPositive(int number) {
-
+    public static void printIsPositive(int number) {
+        boolean result = number>0;
+            System.out.println(result);}
+            public static void main (String[]args){
+                int number=-5;
+            printIsPositive(number);
+        }
     }
-}
+
