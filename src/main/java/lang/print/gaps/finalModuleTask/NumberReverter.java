94809dec981c494dc402ebbeb8baf77140e63a72
Hanna_Urban <huan92@yandex.ru>
2024-01-16 01:45:54 +0300

    done

--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -1,6 +1,15 @@
 package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
-    public void revert(int number) {
+    public static void revert(int number) {
+        int a=number/100;
+        int b=(number-a*100)/10;
+        int c=(number-(a*100+b*10));
+        int result = c*100+b*10+a;
+    System.out.println(result);
+    }
+        public static void main (String[]args){
+            int number=123;
+            revert (number);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/TemperatureConverter.java
@@ -1,6 +1,13 @@
 package lang.print.gaps.finalModuleTask;
 
 public class TemperatureConverter {
-    public void toFahrenheit(int temperatureCelsius){
+    public static void toFahrenheit(float temperatureCelsius){
+        float result = temperatureCelsius*9/5+32;
+        System.out.println(result);}
+    public static void main (String[]args)
+    {
+        float temperatureCelsius=1;
+        toFahrenheit(temperatureCelsius);
     }
 }
+
