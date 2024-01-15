6f0c0fdd916c46a798b8f2da6c23eb409eaabc87
Hanna_Urban <huan92@yandex.ru>
2024-01-16 01:17:56 +0300

    done

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -1,6 +1,17 @@
 package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
-    public void calculateSum(int number) {
+    public static void calculateSum(int number) {
+int a=number/1000;
+int b=(number-a*1000)/100;
+int c=(number-a*1000-b*100)/10;
+int d=number-a*1000-b*100-c*10;
+        int result = a+b+c+d;
+        System.out.println(result);}
+public static void main (String[]args){
+      int number=5432;
+  calculateSum(number);
     }
 }
+//ou will be given a four-digit number your task will be to
+//find the sum of all digits in the given number.
